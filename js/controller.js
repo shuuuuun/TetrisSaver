1c492238249740f4456e1d1c78c14f99b1abbb5d
motoki-shun <motoki-shun@kayac.com>
2015-04-23 01:14:56 +0900

    微修正

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,8 @@
 /*
  キーボードを入力した時に一番最初に呼び出される処理
  */
-document.body.onkeydown = function( e ) {
+// document.body.onkeydown = function( e ) {
+document.onkeydown = function( e ) {
   // キーに名前をセットする
   var keys = {
     37: 'left',  // ←
@@ -12,6 +13,9 @@ document.body.onkeydown = function( e ) {
   };
 
   if ( typeof keys[ e.keyCode ] != 'undefined' ) {
+    
+    e.preventDefault();
+    
     // セットされたキーの場合はtetris.jsに記述された処理を呼び出す
     keyPress( keys[ e.keyCode ] );
     // 描画処理を行う
